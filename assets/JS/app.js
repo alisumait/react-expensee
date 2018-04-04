64577984ef59edf484b9e758588f2e808a721cad
Ali Sumait <34596605+alisumait@users.noreply.github.com>
2018-04-05 01:41:07 +0800

    c

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,52 +1,44 @@
-
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBZ0iMv0Hx9pGRLonPfJ3c4DAKZIBLoiZk",
     authDomain: "expensee-1bf50.firebaseapp.com",
     databaseURL: "https://expensee-1bf50.firebaseio.com",
     projectId: "expensee-1bf50",
     storageBucket: "expensee-1bf50.appspot.com",
     messagingSenderId: "111814420112"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 
 function handleSignUp() {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
-      var repassword= document.getElementById('repassword').value;
-      if (email.length < 4) {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var repassword = document.getElementById('repassword').value;
+    if (email.length < 4) {
         alert('Please enter an email address.');
         return;
-      }
-      if (password.length < 4) {
+    }
+    if (password.length < 4) {
         alert('Please enter a password.');
         return;
-      }
-      if(password != repassword){
-      	alert("Try Again");
-      	return;
-      }
-
-      // Sign in with email and pass.
-      // [START createwithemail]
-      firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
-
-			alert("suceess");
-      }),function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-      };
-      // [END createwithemail]
     }
-   
+    if (password != repassword) {
+        alert("Try Again");
+        return;
+    }
 
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+    var user = firebase.auth().currentUser;
+    
+        alert("success");
+        
+}, function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+});
+
+    // [END createwithemail]
+}
