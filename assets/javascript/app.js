c07b8c67d343ac1278c691ccbf91d6d7de77698d
LAPTOP-6OLBG1GR\User <toufiqurrahman45@gmail.com>
2018-04-08 00:55:01 +0800

    FAQ and Contact
    Bro,done.....
    but having a few prblm in FAQ....i need help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -160,3 +160,13 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   // ...
 });
 }
+
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
